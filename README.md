d3285c5913004d6876d57ef9c5035465eae134b6
Jordan Beland <jobeland@microsoft.com>
2019-12-20 10:58:25 -0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# HexaDb Query UI
-
-A basic UI for expressing the query language on [HexaDb](https://github.com/angshuman/hexadb)
-
-Bring your own HexaDb and try it out at https://jobeland.github.io/hexadb-query-ui
---
+++ precache-manifest.62ce4b6486a7ac6e91a75b97a7213e3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7a595e5495d1509832f10f3361f2d7",
+    "url": "/hexadb-query-ui/index.html"
+  },
+  {
+    "revision": "83bf49f69567ca3b952e",
+    "url": "/hexadb-query-ui/static/css/2.e31a430a.chunk.css"
+  },
+  {
+    "revision": "34805f4593cb45395749",
+    "url": "/hexadb-query-ui/static/css/main.a8dc3214.chunk.css"
+  },
+  {
+    "revision": "83bf49f69567ca3b952e",
+    "url": "/hexadb-query-ui/static/js/2.9d9747b4.chunk.js"
+  },
+  {
+    "revision": "63fa18c955d97788bc2c376b7c1e3125",
+    "url": "/hexadb-query-ui/static/js/2.9d9747b4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34805f4593cb45395749",
+    "url": "/hexadb-query-ui/static/js/main.1f5e456c.chunk.js"
+  },
+  {
+    "revision": "2c6593ca74dfbfecb1fc",
+    "url": "/hexadb-query-ui/static/js/runtime-main.3a12208a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/hexadb-query-ui/static/media/logo.ee7cd8ed.svg"
+  }
+]);
